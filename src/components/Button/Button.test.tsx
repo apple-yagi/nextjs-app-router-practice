102c911e5128bf5454a3513f5b00059e0c1a94ad
apple-yagi <applesryu@gmail.com>
2023-06-08 08:04:44 +0900

    import vitest

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,6 @@
 import { composeStories } from "@storybook/react";
 import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 
 import * as stories from "./Button.stories";
 
