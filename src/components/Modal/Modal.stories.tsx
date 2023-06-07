0c7d025072abd53a5381f5ad546160378491aab7
apple-yagi <applesryu@gmail.com>
2023-06-08 08:35:04 +0900

    Add Modal component

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+
+import { Modal } from ".";
+
+const meta = {
+  component: Modal,
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  args: {
+    trigger: <button>Open Modal</button>,
+    title: "Modal Title",
+    description: "Modal Description",
+  },
+} satisfies Story;
+
+export const Open = {
+  ...Default,
+  play: async ({ canvasElement }) => {
+    const ui = within(canvasElement);
+    await userEvent.click(ui.getByRole("button", { name: "Open Modal" }));
+  },
+} satisfies Story;
