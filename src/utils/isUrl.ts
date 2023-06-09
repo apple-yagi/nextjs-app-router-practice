e219e950d1e29e774f4a4efe2537c27c88fc550a
apple-yagi <applesryu@gmail.com>
2023-06-10 00:55:16 +0900

    Replace var to const

--- src/utils/isUrl.ts
+++ src/utils/isUrl.ts
@@ -1,6 +1,6 @@
 export const isUrl = (str: string) => {
   // URLの正規表現パターン
-  var pattern = new RegExp(
+  const pattern = new RegExp(
     "^(https?:\\/\\/)" + // http:// または https://
       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // ドメイン名
       "((\\d{1,3}\\.){3}\\d{1,3}))" + // IPアドレス
