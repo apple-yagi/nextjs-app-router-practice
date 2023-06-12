f279a95939af114ca2d18c550faad0e905e88f15
apple-yagi <applesryu@gmail.com>
2023-06-12 22:14:16 +0900

    Add TodayRegisterdTechBlogList component

---
+++ src/app/_components/TodayRegisteredTechBlogList/TodayRegisterdTechBlogList.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { TodayRegisterdTechBlogListPresenter } from "./TodayRegisterdTechBlogList.presenter";
+
+const meta = {
+  component: TodayRegisterdTechBlogListPresenter,
+} satisfies Meta<typeof TodayRegisterdTechBlogListPresenter>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  args: {
+    techBlogList: Array.from({ length: 10 }).map((_, i) => ({
+      id: i,
+      title: "新しく自分のブログサイトを作り直しました",
+      link: "https://apple-yagi.me/blog/renewal_my_blog",
+      ogImage:
+        "https://images.microcms-assets.io/assets/a7208e73031e4a0c89212443979ab98a/82716655718e4eef8af491d11a52c66b/renewal_my_blog_ogp.png",
+      createdAt: new Date("2023-06-12T13:02:02.000Z"),
+      updatedAt: new Date("2023-06-12T13:02:02.000Z"),
+    })),
+  },
+} satisfies Story;
