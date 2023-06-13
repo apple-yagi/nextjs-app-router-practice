6e9932bbed8aea0dc758fab7250db440ee538ff7
apple-yagi <applesryu@gmail.com>
2023-06-14 00:07:45 +0900

    Fix layout style

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -4,7 +4,7 @@ import { postTechBlog } from "./_server/postTechBlog.server";
 export default function New() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col">
-      <section className="flex flex-col gap-4 p-24">
+      <section className="flex flex-col gap-4 p-4">
         <h2 className="text-lg">新規追加</h2>
         <TechBlogLinkForm submit={postTechBlog} />
       </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { TodayRegisterdTechBlogList } from "./_components/TodayRegisteredTechBlo
 export default function Home() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col">
-      <section className="flex flex-col gap-4 p-24">
-        <h2 className="text-lg">今日読んだ記事</h2>
+      <section className="flex max-w-5xl flex-col gap-4 p-4">
+        <h2 className="p-4 text-center text-lg">今日読んだ記事</h2>
         <Suspense fallback={<p>...loading</p>}>
           <TodayRegisterdTechBlogList />
         </Suspense>
