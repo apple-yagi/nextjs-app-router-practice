d67e757ed1a8b5312cac63b75e0f4de5db5672c9
apple-yagi <applesryu@gmail.com>
2023-06-15 12:42:08 +0900

    Revert commentout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-// import { TodayRegisteredTechBlogList } from "./_components/TodayRegisteredTechBlogList";
+import { TodayRegisteredTechBlogList } from "./_components/TodayRegisteredTechBlogList";
 
 export default function Home() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col">
       <section className="flex max-w-5xl flex-col gap-4 p-4">
         <h2 className="p-4 text-center text-lg">今日読んだ記事</h2>
-        {/* <TodayRegisteredTechBlogList /> */}
+        <TodayRegisteredTechBlogList />
       </section>
     </main>
   );
