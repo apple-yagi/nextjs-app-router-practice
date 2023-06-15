8aa050088dd2afbcc42984d51c9b3351e147bffa
apple-yagi <applesryu@gmail.com>
2023-06-15 16:43:51 +0900

    test

--- src/app/_components/TodayRegisteredTechBlogList/getTodayRegisteredTechBlogList.server.ts
+++ src/app/_components/TodayRegisteredTechBlogList/getTodayRegisteredTechBlogList.server.ts
@@ -1,11 +1,23 @@
+import { desc, sql } from "drizzle-orm";
+
+import { db, techBlogs } from "@/db";
 import { GetTechBlogListReturn } from "@/server/tech-blogs";
 import { getToday } from "@/utils/getToday";
 
 export const getTodayRegisteredTechBlogList =
   async (): Promise<GetTechBlogListReturn> => {
-    return await (
-      await fetch(`${process.env.APP_URL}/api/tech_blogs?date=${getToday()}`, {
-        cache: "no-store",
-      })
-    ).json();
+    // API経由のデータフェッチ
+    // return await (
+    //   await fetch(`${process.env.APP_URL}/api/tech_blogs?date=${getToday()}`, {
+    //     cache: "no-store",
+    //   })
+    // ).json();
+
+    // DB経由のデータフェッチ
+    return db
+      .select()
+      .from(techBlogs)
+      .where(sql`${techBlogs.readingDate} = ${getToday()}`)
+      .orderBy(desc(techBlogs.createdAt))
+      .limit(20);
   };
--- src/app/_components/TodayRegisteredTechBlogList/index.tsx
+++ src/app/_components/TodayRegisteredTechBlogList/index.tsx
@@ -1,6 +1,7 @@
 import { getTodayRegisteredTechBlogList } from "./getTodayRegisteredTechBlogList.server";
 import { TodayRegisteredTechBlogListPresenter } from "./TodayRegisteredTechBlogList.presenter";
 
+// React Server Components(RSC)
 export const TodayRegisteredTechBlogList = async () => {
   const techBlogList = await getTodayRegisteredTechBlogList();
 
